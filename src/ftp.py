48d539654f5949221691be4db635deb552500bb5
Eugene <eugene.yum@gmail.com>
2022-04-20 22:29:55 -0400

    adding code to convert from dataframe to json object

--- src/ftp.py
+++ src/ftp.py
@@ -5,6 +5,7 @@ Usage:
 python3 ftp.py
 
 """
+import json
 from dotenv import load_dotenv
 import logging
 import os
@@ -36,10 +37,51 @@ def ftp():
     # logging.info('available list of files: ', files)
 
     # read in xlsx as panda
-    dfs = pd.read_excel("./data/sample.xlsx")
-    dfs.to_csv("./data/sample.csv", sep=",")
+    df = pd.read_excel("./data/sample.xlsx", header=None)
 
     # panda to object
+    events = []
+    for index, row in df.iterrows():
+        event = {
+            "id": f"{row[1]} {row[3]} {row[10]}",
+            "date": f"{row[2]} {row[5]}",
+            "county_code": row[1],
+            "hearing_date": row[2],
+            "division": row[3],
+            "judge_code": row[4],
+            "start_time": row[5],
+            "court_room_code": row[7],
+            "hearing_type_code": row[8],
+            "doc_id": row[9],
+            "docket_number": row[10],
+            "case_name": row[11],
+            "litigant": {
+                "entity_id": row[12],
+                "last_name": row[13],
+                "first_name": row[14],
+                "full_name": row[15],
+                "role": {
+                    "code": row[16],
+                    "rank": row[17],
+                    "description": row[29],
+                },
+                "number": row[18],
+            },
+            "attorney": {
+                "entity_id": row[19],
+                "last_name": row[20],
+                "first_name": row[21],
+                "suffix": row[22],
+                "full_name": row[23],
+            },
+            "county_name": row[24],
+            "hearing_type": row[25],
+            "case_type": row[26],
+            "case_status": row[27],
+            "judge_name": row[28],
+            "calendar_id": row[30],
+        }
+        events.append(event)
 
     # write to mongo
 
