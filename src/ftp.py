41ecb1842e87266f8e407722ad4f27bb7af5655a
Eugene <eugene.yum@gmail.com>
2022-04-24 21:43:17 -0400

    still missing ftp retrieval but xlsx to mongo works

--- src/ftp.py
+++ src/ftp.py
@@ -5,11 +5,14 @@ Usage:
 python3 ftp.py
 
 """
-import json
+from datetime import datetime
+import dateutil.tz
 from dotenv import load_dotenv
+import json
 import logging
 import os
 import pandas as pd
+from pymongo import InsertOne, UpdateOne, MongoClient
 import pysftp
 
 logging.basicConfig(level=logging.INFO)
@@ -21,8 +24,22 @@ SFTP_HOST = os.getenv("SFTP_HOST", "gs.judiciary.vermont.gov")
 SFTP_PORT = int(os.getenv("SFTP_PORT", "22"))
 SFTP_USERNAME = os.getenv("SFTP_USERNAME", None)
 SFTP_PASSWORD = os.getenv("SFTP_PASSWORD", None)
+MONGODB_URI = os.getenv("MONGODB_URI")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "events")
+
+def updateOperation(event):
+    """
+    Converts an event into a mongodb bulk write operation
+
+    :param event: event document to inserted to mongo
+    :return: mongodb operation
+    """
+    return UpdateOne({ "_id": event["_id"] }, { "$set": event }, upsert=True)
 
 def ftp():
+    if MONGODB_URI is None:
+        raise Exception("MONGODB_URI environment variable is not set")
+
     # get csv from ftp server
     # logging.info("Downloading file from sftp server")
     # try:
@@ -43,18 +60,31 @@ def ftp():
     events = []
     for index, row in df.iterrows():
         event = {
-            "id": f"{row[1]} {row[3]} {row[10]}",
-            "date": f"{row[2]} {row[5]}",
-            "county_code": row[1],
-            "hearing_date": row[2],
+            "_id": f"{row[1]} {row[3]} {row[10]}",
+            "date": datetime.strptime(f"{row[2]} {row[5]}", '%m/%d/%Y %H:%M').replace(tzinfo=dateutil.tz.gettz('America/New_York')),
+            "county": {
+                "code": row[1],
+                "name": row[24],
+            },
             "division": row[3],
-            "judge_code": row[4],
-            "start_time": row[5],
+            "judge": {
+                "code": row[4],
+                "name": row[28],
+            },
             "court_room_code": row[7],
-            "hearing_type_code": row[8],
+            "hearing": {
+                "date": row[2],
+                "start_time": row[5],
+                "type_code": row[8],
+                "type": row[25],
+            },
             "doc_id": row[9],
             "docket_number": row[10],
-            "case_name": row[11],
+            "case": {
+                "name": row[11],
+                "status": row[27],
+                "type": row[26],
+            },
             "litigant": {
                 "entity_id": row[12],
                 "last_name": row[13],
@@ -74,16 +104,16 @@ def ftp():
                 "suffix": row[22],
                 "full_name": row[23],
             },
-            "county_name": row[24],
-            "hearing_type": row[25],
-            "case_type": row[26],
-            "case_status": row[27],
-            "judge_name": row[28],
             "calendar_id": row[30],
         }
         events.append(event)
 
     # write to mongo
+    client = MongoClient(MONGODB_URI)
+    db = client.courtbot
+    mycol = db[MONGO_COLLECTION]
+    operations = list(map(updateOperation, events))
+    mycol.bulk_write(operations)
 
 if __name__ == "__main__":
     ftp()
